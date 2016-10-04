21d23688e50fca8042f501f0fe8e8ece7106b1e0
Fries <ak532892@gmail.com>
2016-10-05 03:04:23 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,80 +10,91 @@ using namespace std;
 
 int main() {
 	ifstream fin("in.txt");
-	ofstream fout("out.txt");
-	string str;
-	string success("<p class=\"FM-cbox5\"><a href=\"http://home.gamer.com.tw/"),
-		fail("<div class=\"FM-cboxfold");
-	//success ¨S§R¤å, fail §R¤å
-	map<int, string> list, winner;
-	int i, count = 1, del = 0;
+	ofstream fout("out.txt"), fout2("out2.txt");
+	string str, findPeople("<p class=\"FM-cbox5\"><a href=\"http://home.gamer.com.tw/"),
+				noPeople  ("<div class=\"FM-cboxfold");
+	//success æ²’åˆªæ–‡, fail åˆªæ–‡
+	map<int, string> peopleList, winner;
+	int i, count = 1, del = 0, endFloor;
+
+	//æˆªæ­¢æ¨“å±¤
+	cout << "æˆªæ­¢æ¨“å±¤: ";
+	cin >> endFloor;
 
 	while (getline(fin, str)) {
-		if (!str.compare(0, 54, success)) {
+		if (!str.compare(0, 54, findPeople)) {
 			string account = "", id = "";
-			int accountLen;
 
 			for (i = 54; str[i] != '"'; i++)
 				account += str[i];
-			accountLen = account.length();
-			for (i += 23 + accountLen; str[i] != ')'; i++)
+			for (i += (23 + account.length()); str[i] != ')'; i++)
 				id += str[i];
-
-			list[count++] = account + " (" + id + ")";
+			
+			peopleList[count] = account + " (" + id + ")";
+			fout << count << "F " << peopleList[count] << "\t";
+			if (!(count++ % 3))	fout << endl;
 		}
-		else if (!str.compare(0, 23, fail)) {
+		else if (!str.compare(0, 23, noPeople)) {
 			count++;
 			del++;
 		}
+		if (count > endFloor)
+			break;
 	}
 
-	list.erase(list.find(1));
+	count--;
+	peopleList.erase(peopleList.find(1));
 	del++;
-	//±Æ°£¦Û¤v
+	count -= del;
+	//æ’é™¤è‡ªå·±
+	//é¡¯ç¤ºæŠ½çäººæ•¸
+	/*
 	i = 1;
 	for (auto iter = list.begin(); iter != list.end(); iter++, i++) {
 		printf("%3dF %25s\t", iter->first, iter->second.c_str());
 		if (!(i % 3))
 			cout << endl;
-	}
+	}*/
 
 	int totalNum;
 
-	cout << "\n-------------\n¿é¤J©â¼ú¤H¼Æ: ";
+	cout << "\n-------------\nè¼¸å…¥æŠ½çäººæ•¸: ";
 	cin >> totalNum;
 
 	srand(time(NULL));
-	count--;
-	count -= del;
+	
 	set<string> dup;
-	//±Æ°£­«½Æ
+	//æ’é™¤é‡è¤‡
 	for (i = 0; i < totalNum && count; i++) {
 		int luckyNum = (rand() % count);
-		auto iter = next(list.begin(), luckyNum);
 		int setSize = dup.size();
+		auto iter = next(peopleList.begin(), luckyNum);
 
 		dup.insert(iter->second);
 		if (setSize == dup.size()) {
-			list.erase(list.find(iter->first));
+			peopleList.erase(peopleList.find(iter->first));
 			count--;
 			continue;
 		}
 		winner[iter->first] = iter->second;
-		list.erase(list.find(iter->first));
+		peopleList.erase(peopleList.find(iter->first));
 		count--;
 	}
-	cout << "¤¤¼ú¦W³æ:" << endl;
+	cout << "ä¸­çåå–®:" << endl;
 
 	i = 1;
 	for (auto iter = winner.begin(); iter != winner.end(); iter++, i++) {
 		printf("%3dF %25s\t", iter->first, iter->second.c_str());
-		fout << iter->first << "F " << iter->second << endl;
-		if (!(i % 3))
+		fout2 << iter->first << "F " << iter->second << "\t";
+		if (!(i % 3)) {
 			cout << endl;
+			fout2 << endl;
+		}
 	}
 
 	fin.close();
 	fout.close();
+	fout2.close();
 	_getch();
 	return 0;
 }
