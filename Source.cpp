941da4c26bc1a162240443145b90b63aff99ca16
Fries <ak532892@gmail.com>
2016-10-09 01:17:15 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@
 #include<string>
 #include<conio.h>
 #include<fstream>
+#include<sstream>
 #include<iostream>
 using namespace std;
 
@@ -12,10 +13,10 @@ int main() {
 	ifstream fin("網頁原始碼.txt");
 	ofstream fout("抽獎名單.txt"), fout2("中獎名單.txt");
 	string str, findPeople("<p class=\"FM-cbox5\"><a href=\"http://home.gamer.com.tw/"),
-		noPeople("<div class=\"FM-cboxfold");
-	//success 沒刪文, fail 刪文
+			noPeople("class=\"FM-cboxfold\"");
+	//findPeople 沒刪文, noPeople刪文
 	map<int, string> peopleList, winner;
-	int i, count = 1, del = 0, endFloor;
+	int i, count = 1, del = 0, endFloor, cgline = 1, noPeopleLen = noPeople.length();
 
 	//截止樓層
 	cout << "截止樓層: ";
@@ -33,23 +34,53 @@ int main() {
 
 			peopleList[count] = account + " (" + id + ")";
 			fout << count << "F " << peopleList[count] << "\t";
-			if (!(count++ % 3))	fout << endl;
-		}
-		else if (!str.compare(0, 23, noPeople)) {
+			if (!(cgline++ % 3))	fout << endl;
 			count++;
-			del++;
+		}
+		else{
+			if (str.length() >= 61) {
+				if (str.compare(42, noPeopleLen, noPeople) && 
+					str.compare(31, noPeopleLen, noPeople) &&
+					str.compare(5, noPeopleLen, noPeople))
+					continue;
+			}
+			else if (str.length() >= 50) {
+				if (str.compare(31, noPeopleLen, noPeople) &&
+					str.compare(5, noPeopleLen, noPeople))
+					continue;
+			}
+			else if (str.length() >= 24) {
+				if (str.compare(5, noPeopleLen, noPeople))
+					continue;
+			}
+			else continue;
+			istringstream sin(str);
+			string dupDel;
+
+			while (sin >> dupDel) {
+				if (!dupDel.compare(0, noPeopleLen, noPeople)) {
+					count++;
+					del++;
+				}
+			}
 		}
 		if (count > endFloor)
 			break;
 	}
 	cout << "-------------載入結束-------------" << endl;
 	fout.close();
-
 	count--;
-	peopleList.erase(peopleList.find(1));
+
+	//排除自己
+	if(peopleList.find(1) != peopleList.end())
+		peopleList.erase(peopleList.find(1));
 	del++;
 	count -= del;
-	//排除自己
+
+	//驗證一下自己沒有少貼上樓層
+	cout << "總文章: " << count << endl;
+	cout << "自刪 or 被刪文章數: " << del << endl;
+	
 	//顯示抽獎人數
 	/*
 	i = 1;
@@ -57,10 +88,11 @@ int main() {
 	printf("%3dF %25s\t", iter->first, iter->second.c_str());
 	if (!(i++ % 3))
 	cout << endl;
-	}*/
+	}
+	*/
 
 	int totalNum;
-
+	
 	cout << "輸入抽獎人數: ";
 	cin >> totalNum;
 
@@ -95,8 +127,8 @@ int main() {
 			fout2 << endl;
 		}
 	}
-
 	cout << "\n------結束請按任意鍵------" << endl;
+	
 	fin.close();
 	fout2.close();
 	_getch();
